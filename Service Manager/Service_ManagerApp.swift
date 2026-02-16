317951d3ac1a5a9c622465e149e4f8f20251efa5
Adam Farmer <adam@adamfarmer.dev>
2026-02-16 15:20:42 +1100

    Initial Commit

---
+++ Service Manager/Service_ManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Service_ManagerApp.swift
+//  Service Manager
+//
+//  Created by Adam on 16/2/2026.
+//
+
+import SwiftUI
+
+@main
+struct Service_ManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
